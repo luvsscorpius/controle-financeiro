cc82d74f9f20c0e01175d88e58623435b2bda55e
and <andersonvittor1801@gmail.com>
2023-11-24 16:21:58 -0300

    feat(add finance): adding income, expense and total

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import GlobalStyle from './styles/global.js'
 import Header from './components/Header/Header.jsx'
 import Resumo from './components/Resumo/Resumo.jsx'
 import Form from './components/Form/Form.jsx'
 
 const App = () => {
+  const data = localStorage.getItem("transactions")
+  const [transactionsList, setTransactionsList] = useState(
+    //Verifica se existe algum dado no localStorage, se sim ele transforma em JSON, caso não ele retorna uma lista vazia
+    data ? JSON.parse(data) : []
+  )
+  const [Income, setIncome] = useState(0)
+  const [expense, setExpense] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    // filtrando e pegando as saídas e entradas
+    const amountExpense = transactionsList
+      .filter((item) => item.expense)
+      .map((transaction) => Number(transaction.amount))
+
+    const amountIncome = transactionsList
+      .filter((item) => !item.expense)
+      .map((transaction) => Number(transaction.amount))
+
+    // Fazendo um reduce para fazer a soma de todas as saídas
+    const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2)
+    // Fazendo um reduce para fazer a soma de todas as entradas
+    const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2)
+
+    // Somando o total
+    const total = Math.abs(income - expense).toFixed(2)
+
+    setIncome(`R$ ${income}`)
+    setExpense(`R$ ${expense}`)
+    setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`)
+
+  }, [transactionsList])
+
   return (
     <>
       <Header />
-      <Resumo />
+      <Resumo income={Income} expense={expense} total={total} />
       <Form />
       <GlobalStyle />
     </>
--- frontend/src/components/Resumo/Resumo.jsx
+++ frontend/src/components/Resumo/Resumo.jsx
@@ -3,12 +3,12 @@ import * as C from './Styles.jsx'
 import ResumoItem from "../ResumoItem/ResumoItem.jsx";
 import { FaRegArrowAltCircleUp, FaRegArrowAltCircleDown, FaDollarSign } from 'react-icons/fa'
 
-const Resumo = () => {
+const Resumo = ({ income, expense, total }) => {
     return (
         <C.Container>
-            <ResumoItem title="Entradas" Icon={FaRegArrowAltCircleUp} value="1000" />
-            <ResumoItem title="Saídas" Icon={FaRegArrowAltCircleDown} value="1000" />
-            <ResumoItem title="Total" Icon={FaDollarSign} value="1000" />
+            <ResumoItem title="Entradas" Icon={FaRegArrowAltCircleUp} value={income} />
+            <ResumoItem title="Saídas" Icon={FaRegArrowAltCircleDown} value={expense} />
+            <ResumoItem title="Total" Icon={FaDollarSign} value={total} />
         </C.Container>
     )
 }
